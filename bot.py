fb3b550dde341547d8737a7bdea98a8e4eb0ef48
Vale <valerio.pacini@outlook.it>
2025-09-16 20:07:32 +0200

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,13 @@
+importazione OS
+            telegramma.          ext......ext! ext...exr...ext"... Extextext (ext...) ext."... ..." 
+
+"bot_token"
+
+Def. Inizia(Aggiornamento, contesto):
+  aggiornamento. Messaggio.Reply_text("Ciao! Sono il tuo bot per pronostici calcio ⚽")
+
+Aggiornatore = Aggiornamento(Token)
+aggiornamento.spedizioniere..Add_handler(CommandHandler("Inizia", iniziare))
+
+aggiornamento.Start_polling()
+aggiornamento.inattivo..()
